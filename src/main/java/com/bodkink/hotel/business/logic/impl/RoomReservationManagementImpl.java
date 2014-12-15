ed773d114c7666d1d9e2ccabbe79e0177633394f
simonjrp <simon.jrp@gmail.com>
2014-12-15 09:51:53 +0100

    Implemented RoomReservationManagementImpl according to the new service interface

--- src/main/java/com/bodkink/hotel/business/logic/impl/RoomReservationManagementImpl.java
+++ src/main/java/com/bodkink/hotel/business/logic/impl/RoomReservationManagementImpl.java
@@ -4,18 +4,24 @@ package com.bodkink.hotel.business.logic.impl;
 
 import com.bodkink.hotel.business.logic.LogicPackage;
 import com.bodkink.hotel.business.logic.RoomReservationManagement;
+import com.bodkink.hotel.business.model.ReservationStatusEnum;
 import com.bodkink.hotel.business.model.Room;
 import com.bodkink.hotel.business.model.RoomReservation;
 import com.bodkink.hotel.business.model.RoomReservationType;
 import com.bodkink.hotel.business.model.impl.ModelFactoryImpl;
+import com.bodkink.hotel.business.util.EntityToModelConverter;
+import com.bodkink.hotel.business.util.ModelToEntityConverter;
 import com.bodkink.hotel.persistence.IRoomReservationService;
+import com.bodkink.hotel.persistence.model.RoomReservationEntity;
 import com.google.inject.Inject;
+import org.eclipse.emf.common.util.BasicEList;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Date;
+import java.util.List;
 
 /**
  * <!-- begin-user-doc -->
@@ -28,108 +34,141 @@ import java.util.Date;
  */
 public class RoomReservationManagementImpl extends MinimalEObjectImpl.Container implements RoomReservationManagement {
 
-	@Inject
-	IRoomReservationService roomReservationService;
+    @Inject
+    IRoomReservationService roomReservationService;
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	protected RoomReservationManagementImpl() {
-		super();
-	}
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     *
+     * @generated
+     */
+    protected RoomReservationManagementImpl() {
+        super();
+    }
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	protected EClass eStaticClass() {
-		return LogicPackage.Literals.ROOM_RESERVATION_MANAGEMENT;
-	}
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     *
+     * @generated
+     */
+    @Override
+    protected EClass eStaticClass() {
+        return LogicPackage.Literals.ROOM_RESERVATION_MANAGEMENT;
+    }
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 */
-	public EList<RoomReservation> listRoomReservations() {
-		throw new UnsupportedOperationException();
-	}
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated NOT
+     */
+    public EList<RoomReservation> listRoomReservations() {
+        return listRoomReservations(null, null);
+    }
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 */
-	public EList<RoomReservation> listRoomReservations(Date start, Date end) {
-		throw new UnsupportedOperationException();
-	}
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated NOT
+     */
+    public EList<RoomReservation> listRoomReservations(Date start) {
+        return listRoomReservations(start, null);
+    }
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 */
-	public EList<RoomReservation> listRoomReservations(Date start) {
-		throw new UnsupportedOperationException();
-	}
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated NOT
+     */
+    public EList<RoomReservation> listRoomReservations(Date start, Date end) {
+        List<RoomReservationEntity> roomReservationEntityList;
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 */
-	public RoomReservation create(Room room, Date start, Date end, RoomReservationType roomReservationType) {
-		RoomReservation result = ModelFactoryImpl.eINSTANCE.createRoomReservation();
-		result.setRoom(room);
-		result.setStartDate(start);
-		result.setEndDate(end);
-		result.setRoomReservationType(roomReservationType);
+        if (start == null)
+            roomReservationEntityList = roomReservationService.list();
+        else {
+            if (end == null)
+                roomReservationEntityList = roomReservationService.list(start);
+            else
+                roomReservationEntityList = roomReservationService.list(start, end);
+        }
 
-		return result;
-	}
+        EList<RoomReservation> roomReservations = new BasicEList<>(roomReservationEntityList.size());
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 */
-	public RoomReservation findRoomReservation(String roomReservationId) {
-		throw new UnsupportedOperationException();
-	}
+        roomReservationEntityList.forEach(entity -> {
+            roomReservations.add(EntityToModelConverter.convertRoomReservation(entity));
+        });
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 */
-	public boolean cancelRoomReservation(String roomReservationId) {
-		throw new UnsupportedOperationException();
+        return roomReservations;
+    }
 
-		// RoomReservation roomReservation = roomReservationService.find(roomReservationId);
-		// roomReservation.setReservationStatusEnum(ReservationStatusEnum.CANCELED);
-		// return roomReservationService.edit(roomReservation);
-	}
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	public Object eInvoke(int operationID, EList<?> arguments) throws InvocationTargetException {
-		switch (operationID) {
-			case LogicPackage.ROOM_RESERVATION_MANAGEMENT___LIST_ROOM_RESERVATIONS:
-				return listRoomReservations();
-			case LogicPackage.ROOM_RESERVATION_MANAGEMENT___LIST_ROOM_RESERVATIONS__DATE_DATE:
-				return listRoomReservations((Date)arguments.get(0), (Date)arguments.get(1));
-			case LogicPackage.ROOM_RESERVATION_MANAGEMENT___LIST_ROOM_RESERVATIONS__DATE:
-				return listRoomReservations((Date)arguments.get(0));
-			case LogicPackage.ROOM_RESERVATION_MANAGEMENT___CREATE__ROOM_DATE_DATE_ROOMRESERVATIONTYPE:
-				return create((Room)arguments.get(0), (Date)arguments.get(1), (Date)arguments.get(2), (RoomReservationType)arguments.get(3));
-			case LogicPackage.ROOM_RESERVATION_MANAGEMENT___FIND_ROOM_RESERVATION__STRING:
-				return findRoomReservation((String)arguments.get(0));
-			case LogicPackage.ROOM_RESERVATION_MANAGEMENT___CANCEL_ROOM_RESERVATION__STRING:
-				return cancelRoomReservation((String)arguments.get(0));
-		}
-		return super.eInvoke(operationID, arguments);
-	}
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated NOT
+     */
+    public RoomReservation create(Room room, Date start, Date end, RoomReservationType roomReservationType) {
+        RoomReservation result = ModelFactoryImpl.eINSTANCE.createRoomReservation();
+        result.setRoom(room);
+        result.setStartDate(start);
+        result.setEndDate(end);
+        result.setRoomReservationType(roomReservationType);
+
+        return result;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated NOT
+     */
+    public RoomReservation findRoomReservation(String roomReservationId) {
+        RoomReservationEntity entity = roomReservationService.find(roomReservationId);
+        if(entity != null)
+            return EntityToModelConverter.convertRoomReservation(entity);
+        else
+            return null;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated NOT
+     */
+    public boolean cancelRoomReservation(String roomReservationId) {
+        RoomReservationEntity roomReservationEntity = roomReservationService.find(roomReservationId);
+        RoomReservation roomReservation = EntityToModelConverter.convertRoomReservation(roomReservationEntity);
+
+        roomReservation.setReservationStatusEnum(ReservationStatusEnum.CANCELED);
+        roomReservationEntity = ModelToEntityConverter.convertRoomReservation(roomReservation);
+
+        return roomReservationService.edit(roomReservationEntity);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     *
+     * @generated
+     */
+    @Override
+    public Object eInvoke(int operationID, EList<?> arguments) throws InvocationTargetException {
+        switch (operationID) {
+            case LogicPackage.ROOM_RESERVATION_MANAGEMENT___LIST_ROOM_RESERVATIONS:
+                return listRoomReservations();
+            case LogicPackage.ROOM_RESERVATION_MANAGEMENT___LIST_ROOM_RESERVATIONS__DATE_DATE:
+                return listRoomReservations((Date) arguments.get(0), (Date) arguments.get(1));
+            case LogicPackage.ROOM_RESERVATION_MANAGEMENT___LIST_ROOM_RESERVATIONS__DATE:
+                return listRoomReservations((Date) arguments.get(0));
+            case LogicPackage.ROOM_RESERVATION_MANAGEMENT___CREATE__ROOM_DATE_DATE_ROOMRESERVATIONTYPE:
+                return create((Room) arguments.get(0), (Date) arguments.get(1), (Date) arguments.get(2), (RoomReservationType) arguments.get(3));
+            case LogicPackage.ROOM_RESERVATION_MANAGEMENT___FIND_ROOM_RESERVATION__STRING:
+                return findRoomReservation((String) arguments.get(0));
+            case LogicPackage.ROOM_RESERVATION_MANAGEMENT___CANCEL_ROOM_RESERVATION__STRING:
+                return cancelRoomReservation((String) arguments.get(0));
+        }
+        return super.eInvoke(operationID, arguments);
+    }
 
 } //RoomReservationManagementImpl
